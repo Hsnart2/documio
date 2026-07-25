e07200b49da2f8a26a40e618048f9d34831ed2e9
Hsnart2 <97377168+Hsnart2@users.noreply.github.com>
2026-07-25 14:51:05 +0200

    Mount mode-specific automation and practice flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,15 @@ import "./attachment-scroll-fix.css";
 import "./smart-home.css";
 import "./smart-home-auth.css";
 import "./ui-mode.css";
+import "./automation-controls.css";
 import PracticeVisibility from "./PracticeVisibility";
 import PasskeyControls from "./PasskeyControls";
 import GmailSettingsLink from "./GmailSettingsLink";
 import SmartHomeV2 from "./SmartHomeV2";
 import SmartHomeAuthGuard from "./SmartHomeAuthGuard";
 import UiModeController from "./UiModeController";
+import AdvancedEmailAutomation from "./AdvancedEmailAutomation";
+import PostUploadPracticePrompt from "./PostUploadPracticePrompt";
 
 export const metadata: Metadata = {
   title: "Documio",
@@ -35,6 +38,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <UiModeController />
         <SmartHomeAuthGuard />
         <SmartHomeV2 />
+        <AdvancedEmailAutomation />
+        <PostUploadPracticePrompt />
         <PracticeVisibility />
         <PasskeyControls />
         <GmailSettingsLink />
